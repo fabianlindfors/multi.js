34c6e8a16c4083a3e843a482d75ba3bedc1de0c9
Miguel Sanchez Gonzalez <miguelsanychez@gmail.com>
2017-04-01 19:40:00 +0100

    Simplify the appendChild condition

--- src/multi.js
+++ src/multi.js
@@ -59,13 +59,8 @@ var multi = (function() {
             }
 
             // Apply search filtering
-            if ( query ) {
-                if ( label.toLowerCase().indexOf( query.toLowerCase() ) > -1 ) {
-                    select.wrapper.non_selected.appendChild( row );
-                }
-            } else {
+            if ( !query || query && label.toLowerCase().indexOf( query.toLowerCase() ) > -1 ) {
                 select.wrapper.non_selected.appendChild( row );
-
             }
 
         }
